cf07c1b74482c2516617aa2c64cd5bfb4079d9f8
Juiceflavor <danidag516@gmail.com>
2024-08-26 11:10:18 -0500

    Creation of the funtionality getTaskById

--- src/main/java/com/api/kanban_board/controllers/TaskController.java
+++ src/main/java/com/api/kanban_board/controllers/TaskController.java
@@ -2,14 +2,12 @@ package com.api.kanban_board.controllers;
 
 import com.api.kanban_board.dtos.TaskDto;
 import com.api.kanban_board.models.TaskModel;
+import com.api.kanban_board.services.Tasks.GetTaskByIdService;
 import com.api.kanban_board.services.Tasks.SaveTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.api.kanban_board.mappers.TaskMapper.*;
 
@@ -19,11 +17,19 @@ public class TaskController {
 
     @Autowired
     private SaveTaskService saveTaskService;
+    @Autowired
+    private GetTaskByIdService getTaskByIdService;
 
     @PostMapping
     public ResponseEntity<?> saveTask(@RequestBody TaskDto taskDto) {
         TaskModel savedTaskmodel = saveTaskService.execute(toModel(taskDto));
         return new ResponseEntity<>(toDto(savedTaskmodel), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getTaskById(@PathVariable("id") Long id) {
+        TaskModel taskModel = getTaskByIdService.execute(id);
+        return new ResponseEntity<>(toDto(taskModel), HttpStatus.OK);
     }
 
 }
