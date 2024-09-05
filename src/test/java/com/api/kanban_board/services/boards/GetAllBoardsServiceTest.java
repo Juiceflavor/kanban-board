1f0936017c07970d0f8b9dec61526f37be99781b
Juiceflavor <danidag516@gmail.com>
2024-09-05 14:11:14 -0500

    Improvements of code

--- src/test/java/com/api/kanban_board/services/boards/GetAllBoardsServiceTest.java
+++ src/test/java/com/api/kanban_board/services/boards/GetAllBoardsServiceTest.java
@@ -1,5 +1,6 @@
 package com.api.kanban_board.services.boards;
 
+import com.api.kanban_board.MockUtils;
 import com.api.kanban_board.models.BoardModel;
 import com.api.kanban_board.repositories.BoardRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -9,7 +10,6 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
-import static com.api.kanban_board.MockUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -21,35 +21,33 @@ class GetAllBoardsServiceTest {
 
     private List<BoardModel> mockBoardList;
 
-    private List<BoardModel> result;
-
-    private BoardModel board;
+    private MockUtils mockUtils;
 
     @BeforeEach
     void setUp() {
-        // Arrange
         boardRepositoryMock = Mockito.mock(BoardRepository.class);
 
         getAllBoardsService = new GetAllBoardsService(boardRepositoryMock);
 
-        mockBoardList = List.of(makeBoardModelMock());
+        mockUtils = new MockUtils();
+        mockBoardList = List.of(mockUtils.makeBoardModelMock());
     }
 
     @Test
     void shouldGetAllBoardsSuccess() {
+        // Arrange
         Mockito.when(boardRepositoryMock.getAllBoards()).thenReturn(mockBoardList);
 
         // Act
-        result = getAllBoardsService.execute();
-        board = result.get(0);
+        List<BoardModel> result = getAllBoardsService.execute();
 
         // Assert
         assertNotNull(result);
         assertEquals(1, result.size());
-        assertEquals(1L, board.getId());
-        assertEquals("Primer tablero", board.getTitle());
-        assertEquals("Primer tablero", board.getDescription());
-        assertEquals("001", board.getStatus().getCode());
+        assertEquals(1L, result.get(0).getId());
+        assertEquals("Primer tablero", result.get(0).getTitle());
+        assertEquals("Primer tablero", result.get(0).getDescription());
+        assertEquals("001", result.get(0).getStatus().getCode());
     }
 
     @AfterEach
